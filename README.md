9f09b45b2fdbe7b4f5f1548841617c1b9f88035c
Alican Yıldırım <alicanyildirim96@outlook.com>
2021-02-20 23:45:07 +0200

    added hosting

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Netflix Exit Animation
 
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+## [Demo Page](https://netflix-exit-animation.web.app/)
+
+This is a [Next.js](https://nextjs.org/) and [Firebase](https://firebase.google.com/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
 
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,82 +1,59 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../../styles/Home.module.css'
 import React, { useEffect, useState } from "react";
 import Typography from '@material-ui/core/Typography';
 import 'animate.css/animate.css';
-import github_gif from '../assets/to-github.gif';
-import lichess_gif from '../assets/to-lichess.gif';
-
+import github_gif from '../../public/assets/to-github.gif';
+import lichess_gif from '../../public/assets/to-lichess.gif';
 export default function Home() {
-  
-  
   const [isLeaving,setIsLeaving] = useState(false);
-  
   useEffect(function mount() {
     function onBeforeUnload() {
       setIsLeaving(true);
       console.log("unload!");
     }
-
     window.addEventListener("beforeunload", onBeforeUnload);
-
     return function unMount() {
       window.removeEventListener("beforeunload", onBeforeUnload);
     };
   });
-
   if (isLeaving) {
-
     return(
       <div className={styles.container}>
-
         <Head>
           <title>Netflix BeforeUnload</title>
           <link rel="icon" href="/favicon.ico" />
         </Head>
-
         <main className={styles.main}>
-
           <Typography variant="h1" component="h2" className="animate__animated animate__bounceOut animate__slow">
             beforeunload
           </Typography>
         </main>
-      
       </div>
     )
   } else {
-    
     return (
       <div className={styles.container}>
-        
         <Head>
           <title>Netflix BeforeUnload</title>
           <link rel="icon" href="/favicon.ico" />
         </Head>
-
         <main className={styles.main}>
           <h8 className={styles.title}>
             Exit animation with&nbsp;
             <a href="https://developer.mozilla.org/en-US/docs/Web/API/WindowEventHandlers/onbeforeunload" target="_blank">onbeforeunload</a>
           </h8>
-
           <p className={styles.description}>
             How does Netflix do it?&nbsp;
             <a href="https://github.com/alicanyildirim/netflix-exit-animation" target="_blank">
               <code className={styles.code}>on github</code>
             </a>
           </p>
-          
           <div className={styles.grid}>
-
               <div style={{display: "flex", flexDirection: "row"}}>
                 <img src={github_gif} alt="To Github Gif" className={styles.card}/>
                 <img src={lichess_gif} alt="To Lichess Gif" className={styles.card}/>
-
               </div>
-
-
-                
-
               <p className={styles.description}>
                 After you click on another website from the address bar, you should observe the exit animation. 
               </p>
@@ -90,14 +67,9 @@ export default function Home() {
               <p className={styles.description}>
                 You can test the behavior of the animation with different websites.
               </p>
-
-
           </div>
-
         </main>
       </div>
     )
-
   }
-  
 }
